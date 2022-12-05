5e471f1f4e5fd12a20dab618fd71e8d74b7df096
kaviya <kaviyachuddkie07@gmail.com>
2022-12-06 00:13:34 +0530

    Added nested and get single user query

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,20 +1,55 @@
-
 const usersList = [
-  { id: 1, name: "A", age: 2, broken: false },
+  {
+    id: 1,
+    name: "A",
+    age: 2,
+    broken: false,
+    friends: [
+      { id: 2, name: "B", age: 12, broken: false },
+      { id: 8, name: "H", age: 72, broken: false },
+    ],
+  },
   { id: 2, name: "B", age: 12, broken: false },
-  { id: 3, name: "C", age: 22, broken: true },
-  { id: 4, name: "D", age: 32, broken:  false},
+  { id: 3, name: "me", age: 22, broken: true },
+  { id: 4, name: "D", age: 32, broken: false },
   { id: 5, name: "E", age: 42, broken: false },
-  { id: 6, name: "F", age: 52, broken: false },
-  { id: 7, name: "G", age: 62, broken: false },
-  { id: 8, name: "H", age: 72, broken: false },
+  {
+    id: 6,
+    name: "F",
+    age: 52,
+    broken: false,
+    friends: [
+      { id: 3, name: "me", age: 22, broken: true },
+      { id: 8, name: "H", age: 72, broken: false },
+    ],
+  },
+  { id: 7, name: "G", age: 62, broken: false ,friends:[]},
+  {
+    id: 8,
+    name: "H",
+    age: 72,
+    broken: false,
+    friends: [
+      { id: 3, name: "me", age: 22, broken: true },
+      { id: 8, name: "H", age: 72, broken: false },
+    ],
+  },
   { id: 9, name: "I", age: 82, broken: false },
 ];
 
 module.exports = {
-    Query:{
-        users(){
-            return usersList;
-        }
+  Query: {
+    users() {
+      return usersList;
+    },
+    singleUser(parent,args){
+        console.log(
+          "Args",
+          args,
+          usersList.filter((data) => data.id == Number(args.id))
+        );
+        return usersList.filter(data=> data.id == Number(args.id))
     }
-}
+  },
+
+};
--- schema/typedefs.js
+++ schema/typedefs.js
@@ -3,13 +3,17 @@ const typeDefs = gql`
     type user{
         id : ID!
         name : String!
-        age : Int!,
+        age : Int!
+        friends : [user]
         broken : Boolean!
+
     }
     type Query{
         users : [user!]!
+        singleUser(id:ID!) : [user]
     }
 `;
+console.log(typeDefs)
 module.exports = {
   typeDefs
 };
